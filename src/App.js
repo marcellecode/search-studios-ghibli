8ebd347e9105d96988e6d02b7fc79141bffc6eb5
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-18 11:25:32 -0300

    [feat] - basic structure for request api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import './App.css';
-import Header from './components/header/header';
+import "./App.css";
+import { useEffect, useState } from "react";
+import Header from "./components/header/header";
 
 function App() {
+  const [filmsList, setFilmsList] = useState();
+
+  useEffect(() => {
+    let mounted = true;
+    fetch("https://ghibliapi.herokuapp.com/films/")
+      .then((response) => response.json())
+      .then((data) => {
+        if(mounted) {
+          setFilmsList(data);
+        }
+      });
+    return () => (mounted = false);
+  }, []);
+
+  console.log(filmsList);  
   return (
-    <div className="App">
+    <div className="App"> 
       <Header />
     </div>
   );
