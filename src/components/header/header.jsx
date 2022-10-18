efc6dc0a0779adc751d03efa4650b174d56610ce
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-18 14:03:46 -0300

    [feat] - create header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,23 +1,60 @@
-// import logo from '../../logo.svg';
-import './header.css';
+import "./header.css";
+import logo from "../../general/images/Studio_Ghibli_logo.png";
+import { FaBars } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 const Header = () => {
-    return(
-        <header className="header-container">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+  const [menuStatus, setMenuStatus] = useState(false);
+
+  useEffect(() => {
+    if (menuStatus) {
+      document
+        .getElementsByClassName("header-container__menu-content")[0]
+        .classList.add("expanded");
+    } else {
+      document
+        .getElementsByClassName("header-container__menu-content")[0]
+        .classList.remove("expanded");
+    }
+  });
+
+  return (
+    <header className="header-container">
+      <img src={logo} className="header-logo" alt="Studios Ghibli Logo" />
+      <div id="menu" className="header-container__menu-content">
         <a
-          className="App-link"
+          className="header-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Filmes
         </a>
-      </header>
-    );
-}
+        <a
+          className="header-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Atores
+        </a>
+        <a
+          className="header-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Locais
+        </a>
+      </div>
+      <button
+        className="hamburguer-icon"
+        onClick={() => setMenuStatus(!menuStatus)}
+      >
+        <FaBars />
+      </button>
+    </header>
+  );
+};
 
 export default Header;
