95906e32b76a4a0d00841fef8b1e8161d83b4fbf
Marcelle Vargas da Silva <marcellecode@gmail.com>
2022-10-19 16:36:21 -0300

    [fix] - Fix header component

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,22 +7,10 @@ import { Link } from "react-router-dom";
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
 
-  useEffect(() => {
-    if (isOpen) {
-      document
-        .getElementsByClassName("header-container__menu-content")[0]
-        .classList.add("expanded");
-    } else {
-      document
-        .getElementsByClassName("header-container__menu-content")[0]
-        .classList.remove("expanded");
-    }
-  });
-
   return (
     <header className="header-container">
       <img src={logo} className="header-logo" alt="Studios Ghibli Logo" />
-      <div id="menu" className="header-container__menu-content">
+      <div id="menu" className={`header-container__menu-content ${(isOpen ? 'expanded' :  '')}`}>
         <Link className="header-link" to="/">
           Filmes
         </Link>
